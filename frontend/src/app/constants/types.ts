0e23002bc118aa5909b0190ee6962c3880d9d5c7
SuperPhantom <super.phantom.dev@gmail.com>
2023-06-10 01:16:20 -0700

    constants

---
+++ frontend/src/app/constants/types.ts
@@ -0,0 +1,15 @@
+
+export type ImageInfoType = {
+  index?: number;
+  title?: string;
+  src?: string;
+}
+
+export const API_BASE_URL = "http://3.144.89.157/";
+// export const API_BASE_URL = "http://localhost:8080/";
+
+export const SolutionViewLayout = {
+      both: 3,
+  only_img: 2,
+  only_txt: 1
+}
